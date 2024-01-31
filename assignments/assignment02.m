da3887db44dac9f5d19495920a64c07bfd5a8eb4
Aidan Cusa <aidan.cusa@cooper.edu>
2024-01-31 11:45:03 -0500

    finished assignment 2

--- assignments/assignment02.m
+++ assignments/assignment02.m
@@ -4,11 +4,12 @@
 % Copyright (C) 2024 Aidan Cusa <aidancusa@gmail.com>
 
 clc;    % clear command window
-clear;  % clear all variables from current workspace
+clear;  % clear all variables from current worwkspace
+close all; 
 
 %% part 1
 u = -4:2:4;
-v = 0:(pi/4):pi;
+v = 0:(pi/4):pi; % could also use linspace(0, pi, 5) but meh
 
 %% part 2
 f = prod(1:10);
@@ -20,15 +21,23 @@ A = zeros(2, 4);
 A(1, 1) = 1;
 A(2, 3) = 1; 
 
-% display(A)
-
 % part B
-b = reshape(1:16, 2, [])';
-B = reshape(b, [4, 4]);
+b = reshape(1:16, 2, [])'; % reshape 1-16 into two rows then transpose it to 
+                           % become one even column and one odd column
+
+B = reshape(b, [4, 4]);    % now reshape the two columns into a 4x4 matrix
+                           % will get desired result due to column major
+                           % ordering
 
 %% part 4
+n = 0:50; 
+t = linspace(-pi, pi, 1000);
 
-t = linspace(-pi, pi, 50);
+a_n = (2 * n + 1)';   % transpose in order to make a_n into a column vector
+                      % so that it can be multiplied by t properly
 
+s = sum(sin(a_n * t) ./ a_n);  % element wise division for dimensions to
+                               % match up
 
+plot(t, s)
 
