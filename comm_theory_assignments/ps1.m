6a89ac0510d499b0ace80381946e7aa23a773e10
Aidan Cusa <aidancusa@gmail.com>
2025-09-17 19:29:14 -0400

    Added ps1 for comm theory

---
+++ comm_theory_assignments/ps1.m
@@ -0,0 +1,10 @@
+% SPDX-License-Identifier: GPL-3.0-or-later
+%
+% ECE300 Communication Theory - Problem Set 1
+% Copyright (C) 2025 Aidan Cusa <aidancusa@gmail.com>
+
+clc;    % clear command window
+clear;  % clear all variables from current worwkspace
+close all; 
+
+
