5d439cc48120704a16fb47d4ce9d5fa50f4facdb
Aidan Cusa <aidancusa@gmail.com>
2025-11-12 12:28:14 -0500

    Finished question 2 for ps4

---
+++ comm_theory_assignments/ps4.m
@@ -0,0 +1,44 @@
+% SPDX-License-Identifier: GPL-3.0-or-later
+%
+% ECE300 Communication Theory - Problem Set 3
+% Copyright (C) 2025 Aidan Cusa <aidancusa@gmail.com>
+
+clc; clear; close all; 
+
+%% Question 2
+amplitudes = [2, 10, 2];
+bandwidths = [10, 2, 20];
+kf = 2;
+
+n = numel(amplitudes);
+delta_f = zeros(n,1);
+mod_index = zeros(n,1);
+carson_bw = zeros(n,1);
+universal_bw = zeros(n,1);
+band_type = strings(n,1);
+
+for i = 1:n
+    [delta_f(i), mod_index(i), band_type(i), carson_bw(i)] = get_fm_results(kf, amplitudes(i), bandwidths(i));
+end
+
+% inserting universal curve values based on inspection from chart in the
+% notes => gets normalized bandwidth B_T / delta_f at a particular beta.
+universal_bw(1) = 10 * delta_f(1); % norm bandwidth @ B = 0.4 approx. 10
+universal_bw(2) = 3 * delta_f(2); % norm bandwidth @ B = 10 approx. 3
+universal_bw(3) = 16 * delta_f(3); % norm bandwidth @ B = 0.2 approx. 16
+
+Summary = table( (1:n).', amplitudes.', delta_f, mod_index, band_type, carson_bw, universal_bw, ...
+                 'VariableNames', {'Case','amplitude','delta_f','mod. index','band type','CarsonBW (MHz)','UniversalBW (MHz)'});
+
+disp(Summary);
+
+function [delta_f, mod_index, band_type, carson_bw] = get_fm_results(kf, amplitude, bandwidth)
+    delta_f = kf * amplitude;
+    mod_index = delta_f / bandwidth;
+    if mod_index < 1
+        band_type = 'Narrowband';
+    else
+        band_type = "Wideband";
+    end
+    carson_bw = 2 * (delta_f + bandwidth);
+end
